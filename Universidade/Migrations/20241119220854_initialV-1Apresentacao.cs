169eca27d6beb2346064f5e9ea22db28203bbd4f
LuizHenriqueBarbosaDaSilva <luizbarbosa785@gmail.com>
2024-11-19 19:26:58 -0300

    bug fixes

---
+++ Universidade/Migrations/20241119220854_initialV-1Apresentacao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Universidade.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialV1Apresentacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Disciplinas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DataRegistro",
+                value: new DateTime(2024, 11, 19, 19, 8, 54, 400, DateTimeKind.Local).AddTicks(3261));
+
+            migrationBuilder.UpdateData(
+                table: "Disciplinas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DataRegistro",
+                value: new DateTime(2024, 11, 19, 19, 8, 54, 400, DateTimeKind.Local).AddTicks(3263));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Disciplinas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DataRegistro",
+                value: new DateTime(2024, 11, 18, 22, 1, 51, 696, DateTimeKind.Local).AddTicks(8468));
+
+            migrationBuilder.UpdateData(
+                table: "Disciplinas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DataRegistro",
+                value: new DateTime(2024, 11, 18, 22, 1, 51, 696, DateTimeKind.Local).AddTicks(8470));
+        }
+    }
+}
